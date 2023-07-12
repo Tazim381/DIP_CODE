91483fd9c49269b0c13726d389153ecaad2eb1f8
Tazim <70653485+Tazim381@users.noreply.github.com>
2023-07-12 11:40:13 +0600

    Add files via upload
    chap 1-7 added

---
+++ Chapter 5/11.BilateralFilter.py
@@ -0,0 +1,16 @@
+import numpy as np
+import skimage.io as io
+import skimage.filters.rank as rk
+import scipy.ndimage as ndi
+import matplotlib.pyplot as plt
+import skimage.restoration as re
+
+
+
+
+k= io.imread('C://DIP//Dataset//7.2.01.tiff')
+cb=re.denoise_bilateral(k,win_size=7,sigma_spatial=10)
+
+io.imshow(cb)
+io.show()
+
